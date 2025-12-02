e86448ccc4a5baad5077d8285834d9b1c485b116
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2025-12-02 17:47:49 +0530

    Update gate_primitives_tb.v

--- Day1/gate_primitives/gate_primitives_tb.v
+++ Day1/gate_primitives/gate_primitives_tb.v
@@ -24,3 +24,12 @@ initial begin
 	$monitor("@time=%0t a=%b b=%b and_ab=%b or_ab=%b not_b=%b nand_ab=%b nor_ab=%b xor_ab=%b xnor_ab=%b buf_b=%b",$time,a,b,y1,y2,y3,y4,y5,y6,y7,y8);
 end
 endmodule
+
+/*
+* Output
+* @time=0 a=0 b=0 and_ab=0 or_ab=0 not_b=1 nand_ab=1 nor_ab=1 xor_ab=0 xnor_ab=1 buf_b=0
+* @time=5 a=0 b=1 and_ab=0 or_ab=1 not_b=0 nand_ab=1 nor_ab=0 xor_ab=1 xnor_ab=0 buf_b=1
+* @time=10 a=1 b=0 and_ab=0 or_ab=1 not_b=1 nand_ab=1 nor_ab=0 xor_ab=1 xnor_ab=0 buf_b=0
+* @time=15 a=1 b=1 and_ab=1 or_ab=1 not_b=0 nand_ab=0 nor_ab=0 xor_ab=0 xnor_ab=1 buf_b=1
+* gate_primitives_tb.v:21: $finish called at 20 (1s)
+*/
