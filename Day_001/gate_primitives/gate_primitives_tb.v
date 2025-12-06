b594dbee7d01d2503bafffa29c90e6c7bbed109a
sanket112k <sanket112k@gmail.com>
2025-12-06 10:48:58 +0530

    updating gate_primitives_tb

--- Day_001/gate_primitives/gate_primitives_tb.v
+++ Day_001/gate_primitives/gate_primitives_tb.v
@@ -14,22 +14,32 @@ gate_primitives dut(
 	.y8(y8)
 );
 initial begin
-	a=1'b0;b=1'b0;#5  
-	a=1'b0;b=1'b1;#5 
-	a=1'b1;b=1'b0;#5 
-	a=1'b1;b=1'b1;#5 
+    a=1'b0;b=1'b0;#5
+    a=1'b0;b=1'b1;#5
+    a=1'b1;b=1'b0;#5
+    a=1'b1;b=1'b1;#5
+    a=1'b0;b=1'bx;#5
+    a=1'b1;b=1'bx;#5
+    a=1'b0;b=1'bz;#5
+    a=1'b1;b=1'bz;#5
+    a=1'bx;b=1'bz;#5 
 	$finish;
 end
 initial begin
-	$monitor("@time=%0t a=%b b=%b and_ab=%b or_ab=%b not_b=%b nand_ab=%b nor_ab=%b xor_ab=%b xnor_ab=%b buf_b=%b",$time,a,b,y1,y2,y3,y4,y5,y6,y7,y8);
+	$monitor("a=%b b=%b and_ab=%b or_ab=%b not_b=%b nand_ab=%b nor_ab=%b xor_ab=%b xnor_ab=%b buf_b=%b",a,b,y1,y2,y3,y4,y5,y6,y7,y8);
 end
 endmodule
 
 /*
 * Output
-* @time=0 a=0 b=0 and_ab=0 or_ab=0 not_b=1 nand_ab=1 nor_ab=1 xor_ab=0 xnor_ab=1 buf_b=0
-* @time=5 a=0 b=1 and_ab=0 or_ab=1 not_b=0 nand_ab=1 nor_ab=0 xor_ab=1 xnor_ab=0 buf_b=1
-* @time=10 a=1 b=0 and_ab=0 or_ab=1 not_b=1 nand_ab=1 nor_ab=0 xor_ab=1 xnor_ab=0 buf_b=0
-* @time=15 a=1 b=1 and_ab=1 or_ab=1 not_b=0 nand_ab=0 nor_ab=0 xor_ab=0 xnor_ab=1 buf_b=1
-* gate_primitives_tb.v:21: $finish called at 20 (1s)
+a=0 b=0 and_ab=0 or_ab=0 not_b=1 nand_ab=1 nor_ab=1 xor_ab=0 xnor_ab=1 buf_b=0
+a=0 b=1 and_ab=0 or_ab=1 not_b=0 nand_ab=1 nor_ab=0 xor_ab=1 xnor_ab=0 buf_b=1
+a=1 b=0 and_ab=0 or_ab=1 not_b=1 nand_ab=1 nor_ab=0 xor_ab=1 xnor_ab=0 buf_b=0
+a=1 b=1 and_ab=1 or_ab=1 not_b=0 nand_ab=0 nor_ab=0 xor_ab=0 xnor_ab=1 buf_b=1
+a=0 b=x and_ab=0 or_ab=x not_b=x nand_ab=1 nor_ab=x xor_ab=x xnor_ab=x buf_b=x
+a=1 b=x and_ab=x or_ab=1 not_b=x nand_ab=x nor_ab=0 xor_ab=x xnor_ab=x buf_b=x
+a=0 b=z and_ab=0 or_ab=x not_b=x nand_ab=1 nor_ab=x xor_ab=x xnor_ab=x buf_b=x
+a=1 b=z and_ab=x or_ab=1 not_b=x nand_ab=x nor_ab=0 xor_ab=x xnor_ab=x buf_b=x
+a=x b=z and_ab=x or_ab=x not_b=x nand_ab=x nor_ab=x xor_ab=x xnor_ab=x buf_b=x
+gate_primitives_tb.v:26: $finish called at 45 (1s)
 */
