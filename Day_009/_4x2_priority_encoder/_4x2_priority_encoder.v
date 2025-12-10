0f25d188d18b4cea4faac6329f27292acbdfbab5
sanket112k <sanket112k@gmail.com>
2025-12-10 14:28:40 +0530

    updating _4x2_priority_encoder

--- Day_009/_4x2_priority_encoder/_4x2_priority_encoder.v
+++ Day_009/_4x2_priority_encoder/_4x2_priority_encoder.v
@@ -1,11 +1,17 @@
 module _4x2_priority_encoder (
     input [3:0]in,
-    output reg [1:0]out
+    output reg [1:0]out,
+    output reg valid
 );
 integer i;
 always @(*) begin
     for(i=0;i<4;i=i+1) begin
-        if(in[i]===1'b1) begin
+        if(in==0) begin 
+            valid=0;
+            out={2{1'bx}};
+        end
+        else if(in[i]===1'b1) begin
+            valid=1;
             out<=i;
             i<=4;
         end
--- Day_009/_4x2_priority_encoder/_4x2_priority_encoder_tb.v
+++ Day_009/_4x2_priority_encoder/_4x2_priority_encoder_tb.v
@@ -1,26 +1,30 @@
 module _4x2_priority_encoder_tb;
 reg [3:0]in;
 wire [1:0]out;
+wire valid;
 integer i;
-_4x2_priority_encoder dut(in,out);
+_4x2_priority_encoder dut(in,out,valid);
 initial begin
-    in={4{1'b0}};
+    in={4{1'b0}};#10
     for(i=0;i<4;i=i+1) begin
         in[i]=1'b1;in[i-1]=1'bx;#10;
     end
+    in={4{1'b0}};#10
     $finish;
 end
 initial begin
-    $monitor("input=%b output=%b",in,out);
+    $monitor("input=%b valid=%b output=%b",in,valid,out);
 end
 endmodule
 
 /*
 * OUTPUT
-input=0001 output=00
-input=001x output=01
-input=01xx output=10
-input=1xxx output=11
-_4x2_priority_encoder_tb.v:11: $finish called at 40 (1s)
+input=0000 valid=0 output=xx
+input=0001 valid=1 output=00
+input=001x valid=1 output=01
+input=01xx valid=1 output=10
+input=1xxx valid=1 output=11
+input=0000 valid=0 output=xx
+_4x2_priority_encoder_tb.v:13: $finish called at 60 (1s)
 * 
 */
