e6ccaa1af0a0c66c83f5198bf17b02f68e50d9dc
sanket112k <sanket112k@gmail.com>
2025-12-12 09:57:40 +0530

    updating _4x1_using_2x1_muxw

--- Day_011/_4x1_using_2x1_mux/_4x1_using_2x1_mux.v
+++ Day_011/_4x1_using_2x1_mux/_4x1_using_2x1_mux.v
@@ -3,7 +3,10 @@ module _4x1_using_2x1_mux(
     input [1:0]s,
     output out
 );
-assign out=in[s];
+wire [1:0]w;
+_2x1_mux dut1(.in(in[3:2]),.s(s[0]),.out(w[1]));
+_2x1_mux dut2(.in(in[1:0]),.s(s[0]),.out(w[0]));
+_2x1_mux dut3(.in(w),.s(s[1]),.out(out));
 endmodule
 
 module _2x1_mux(
