495a14e8a173850df464921663e3c89ef1686ce6
sanket112k <sanket112k@gmail.com>
2025-12-22 15:49:59 +0530

    updated tff_tb.v

--- Day_018/tff/tff_tb.v
+++ Day_018/tff/tff_tb.v
@@ -28,31 +28,29 @@ endmodule
 
 /*
 * OUTPUT
-VCD info: dumpfile srff.vcd opened for output.
-                   0 clk=0 reset=0 s=0 r=1 q=x
-                   3 clk=0 reset=0 s=0 r=0 q=x
-                   5 clk=1 reset=0 s=0 r=0 q=x
-                  10 clk=0 reset=0 s=0 r=0 q=x
-                  12 clk=0 reset=0 s=0 r=1 q=x
-                  15 clk=1 reset=0 s=1 r=0 q=1
-                  18 clk=1 reset=0 s=0 r=0 q=1
-                  20 clk=0 reset=0 s=0 r=0 q=1
-                  24 clk=0 reset=0 s=0 r=1 q=1
-                  25 clk=1 reset=0 s=0 r=1 q=0
-                  28 clk=1 reset=1 s=0 r=1 q=0
-                  30 clk=0 reset=1 s=1 r=0 q=0
-                  33 clk=0 reset=1 s=0 r=1 q=0
-                  35 clk=1 reset=1 s=0 r=1 q=0
-                  36 clk=1 reset=1 s=1 r=0 q=0
-                  38 clk=1 reset=0 s=1 r=0 q=0
-                  40 clk=0 reset=0 s=1 r=0 q=0
-                  45 clk=1 reset=0 s=0 r=1 q=0
-                  50 clk=0 reset=0 s=0 r=1 q=0
-                  51 clk=0 reset=0 s=0 r=0 q=0
-                  54 clk=0 reset=0 s=1 r=0 q=0
-                  55 clk=1 reset=0 s=1 r=0 q=1
-                  57 clk=1 reset=0 s=0 r=0 q=1
-srff_tb.v:14: $finish called at 60 (1s)
-                  60 clk=0 reset=0 s=0 r=0 q=1
+VCD info: dumpfile tff.vcd opened for output.
+                   0 clk=0 reset=1 t=1 q=x
+                   3 clk=0 reset=1 t=0 q=x
+                   5 clk=1 reset=1 t=0 q=0
+                   6 clk=1 reset=0 t=0 q=0
+                  10 clk=0 reset=0 t=0 q=0
+                  12 clk=0 reset=0 t=1 q=0
+                  15 clk=1 reset=0 t=0 q=0
+                  20 clk=0 reset=0 t=0 q=0
+                  24 clk=0 reset=0 t=1 q=0
+                  25 clk=1 reset=0 t=1 q=1
+                  28 clk=1 reset=1 t=1 q=1
+                  30 clk=0 reset=1 t=0 q=1
+                  33 clk=0 reset=1 t=1 q=1
+                  34 clk=0 reset=0 t=1 q=1
+                  35 clk=1 reset=0 t=1 q=0
+                  36 clk=1 reset=0 t=0 q=0
+                  40 clk=0 reset=0 t=0 q=0
+                  45 clk=1 reset=0 t=1 q=1
+                  50 clk=0 reset=0 t=1 q=1
+                  51 clk=0 reset=0 t=0 q=1
+                  55 clk=1 reset=0 t=0 q=1
+tff_tb.v:14: $finish called at 60 (1s)
+                  60 clk=0 reset=0 t=0 q=1
 * 
 */
