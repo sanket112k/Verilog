8b64e69e474413b989eb20780969a7f843064daf
sanket112k <sanket112k@gmail.com>
2025-12-24 12:01:45 +0530

    create _4_bit_sync_down_counter

---
+++ Day_022/_4_bit_sync_down_counter/_4_bit_sync_down_counter.v
@@ -0,0 +1,13 @@
+module _4_bit_sync_down_counter(
+    input clk,
+    input reset,    //sync reset
+    output reg [3:0]count
+);
+initial begin
+    count = 0;
+end
+always @(posedge clk) begin
+    if(reset) count = 0;
+    else count = count - 1'b1;
+end
+endmodule
