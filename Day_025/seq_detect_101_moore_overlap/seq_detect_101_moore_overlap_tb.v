943eeefa4c84aaff992ef5a9114120a222c61336
sanket112k <sanket112k@gmail.com>
2025-12-27 23:25:42 +0530

    created seq_detect_101_moore_overlap

---
+++ Day_025/seq_detect_101_moore_overlap/seq_detect_101_moore_overlap_tb.v
@@ -0,0 +1,38 @@
+module seq_detect_101_moore_overlap_tb;
+reg clk, reset, in;
+wire out;
+
+seq_detect_101_moore_overlap dut(clk, reset, in, out);
+
+always #5 clk = ~clk;
+
+initial begin
+    clk = 0;
+    reset = 1;
+    repeat (5) @(negedge clk) begin
+        reset = 0;
+        in = $urandom_range(1'b0,1'b1);
+    end
+
+    repeat (2) @(negedge clk) reset = 1;
+
+    repeat (8) @(negedge clk) begin
+        reset = 0;
+        in = ~in;
+    end
+
+    repeat (50) @(negedge clk) begin
+        reset = 0;
+        in = $urandom_range(1'b0,1'b1);
+    end
+    $finish;
+end
+initial begin
+    $monitor("t=%0t clk=%b reset=%b in=%b out=%b", $time, clk, reset, in, out);
+    $dumpfile("seq_detect_101_moore_overlap.vcd");
+    $dumpvars(0,seq_detect_101_moore_overlap_tb);
+end
+endmodule
+
+/*
+* OUTPUT
