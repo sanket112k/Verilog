8208335e8444a843e42dce98b94403ea78637ec2
sanket112k <sanket112k@gmail.com>
2025-12-27 18:46:58 +0530

    created seq_detect_101_moore_nonoverlap

---
+++ Day_025/seq_detect_101_moore_nonoverlap/seq_detect_101_moore_nonoverlap.v
@@ -0,0 +1,24 @@
+module seq_detect_101_moore_nonoverlap(
+    input clk, reset, in,
+    output out
+);
+
+reg [1:0]state, next_state;
+parameter A = 2'd0, B = 2'd1, C = 2'd2, D = 2'd3;
+
+always @(*) begin
+    case(state)
+        A: next_state = in ? B : A;
+        B: next_state = in ? B : C;
+        C: next_state = in ? D : A;
+        D: next_state = in ? B : A;
+    endcase
+end
+
+always @(posedge clk) begin
+    if(reset) state <= A;
+    else state <= next_state;
+end
+
+assign out = (state == D);
+endmodule
