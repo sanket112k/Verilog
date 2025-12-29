6ee7d48fd143ca4156c9d3780859065df554ad3a
sanket112k <sanket112k@gmail.com>
2025-12-29 10:29:00 +0530

    created seq_detect_110110

--- Day_027/seq_detect_110110/seq_detect_110110.v
+++ Day_027/seq_detect_110110/seq_detect_110110.v
@@ -6,7 +6,12 @@ module seq_detect_110110_moore_nonoverlap(
 reg [2:0] state, next_state;
 parameter [2:0] A = 3'd0, B = 3'd1, C = 3'd2, D = 3'd3, E = 3'd4, F = 3'd5, G = 3'd6;
 
-always @(*) begin
+always @(posedge clk) begin
+    if(reset) state <= A;
+    else state <= next_state;
+end
+
+always @(state or in) begin
     case(state)
         A: next_state = in ? B : A;
         B: next_state = in ? C : A;
@@ -17,11 +22,6 @@ always @(*) begin
         G: next_state = in ? B : A;
         default: next_state = 3'dx;
     endcase
-end
-
-always @(posedge clk) begin
-    if(reset) state <= A;
-    else state <= next_state;
 end
 
 assign out = (state == G);
@@ -37,7 +37,12 @@ module seq_detect_110110_moore_overlap(
 reg [2:0] state, next_state;
 parameter [2:0] A = 3'd0, B = 3'd1, C = 3'd2, D = 3'd3, E = 3'd4, F = 3'd5, G = 3'd6;
 
-always @(*) begin
+always @(posedge clk) begin
+    if(reset) state <= A;
+    else state <= next_state;
+end
+
+always @(state or in) begin
     case(state)
         A: next_state = in ? B : A;
         B: next_state = in ? C : A;
@@ -45,14 +50,9 @@ always @(*) begin
         D: next_state = in ? E : A;
         E: next_state = in ? F : B;
         F: next_state = in ? C : G;
-        G: next_state = in ? B : D;
+        G: next_state = in ? E : A;
         default: next_state = 3'dx;
     endcase
-end
-
-always @(posedge clk) begin
-    if(reset) state <= A;
-    else state <= next_state;
 end
 
 assign out = (state == G);
@@ -73,7 +73,7 @@ always @(posedge clk) begin
     else state <= next_state;
 end
 
-always @(*) begin
+always @(state or in) begin
     case(state)
         A: begin next_state = in ? B : A; out = 1'b0; end
         B: begin next_state = in ? C : A; out = 1'b0; end
@@ -102,7 +102,7 @@ always @(posedge clk) begin
     else state <= next_state;
 end
 
-always @(*) begin
+always @(state or in) begin
     case(state)
         A: begin next_state = in ? B : A; out = 1'b0; end
         B: begin next_state = in ? C : A; out = 1'b0; end
