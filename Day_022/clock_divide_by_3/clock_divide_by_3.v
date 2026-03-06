5987db936511b8513bb5fd83eee4cbd5ae832963
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-03-06 10:11:02 +0530

    Update clock_divide_by_3.v

--- Day_022/clock_divide_by_3/clock_divide_by_3.v
+++ Day_022/clock_divide_by_3/clock_divide_by_3.v
@@ -4,8 +4,9 @@ module clock_divide_by_3(
 );
 reg [1:0]count = 2'd0;
 always @(posedge clk_in) begin
-    count <= count + 2'd1;
-    if(count == 2'd2) count <= 2'd0;
+    if(count == 2'd2)
+        count <= 2'd0;
     clk_out <= count[1];
+    count   <= count + 2'd1;
 end
 endmodule
