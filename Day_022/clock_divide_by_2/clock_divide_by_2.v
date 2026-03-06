300c5cf45c173b71aa33eda8681fcfea9e7f2f51
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-03-06 10:07:21 +0530

    Update clock_divide_by_2.v

--- Day_022/clock_divide_by_2/clock_divide_by_2.v
+++ Day_022/clock_divide_by_2/clock_divide_by_2.v
@@ -2,9 +2,7 @@ module clock_divide_by_2(
     input clk_in,
     output reg clk_out
 );
-reg count=0;
 always @(posedge clk_in) begin
-    count <= ~count;
-    clk_out <= count;
+    clk_out <= ~clk_out;
 end
 endmodule
