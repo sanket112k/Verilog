d79ff040446d49d16ae9b49a7d764b48f6fce049
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-03-06 10:48:49 +0530

    Update mod_n_counter.v

--- Day_021/mod_n_counter/mod_n_counter.v
+++ Day_021/mod_n_counter/mod_n_counter.v
@@ -2,7 +2,6 @@ module mod_n_counter #(parameter N=4)(
     input clk,
     input reset,    //sync reset
 );
-    
 initial count = 0;
 always @(posedge clk) begin
     if (reset || count == N-1) count = 0;
