f0a95d34584f99235aedb5dcba448610a7ad8054
Sanket Pawar H <79686152+sanket112k@users.noreply.github.com>
2026-03-07 16:19:43 +0530

    Update synchronous_fifo_tb.v

--- synchronous_fifo/synchronous_fifo_tb.v
+++ synchronous_fifo/synchronous_fifo_tb.v
@@ -86,3 +86,39 @@ initial begin
     $dumpvars(0, synchronous_fifo_tb);
 end
 endmodule
+
+/*
+OUTPUT:
+Time=0 | w_en=0 r_en=0 data_in=00 data_out=xx full=x empty=x
+Time=5000 | w_en=0 r_en=0 data_in=00 data_out=00 full=0 empty=1
+Writing to FIFO
+Time=25000 | w_en=1 r_en=0 data_in=24 data_out=00 full=0 empty=0
+Time=35000 | w_en=1 r_en=0 data_in=81 data_out=00 full=0 empty=0
+Time=45000 | w_en=1 r_en=0 data_in=09 data_out=00 full=0 empty=0
+Time=55000 | w_en=1 r_en=0 data_in=63 data_out=00 full=0 empty=0
+Time=65000 | w_en=1 r_en=0 data_in=0d data_out=00 full=0 empty=0
+Time=75000 | w_en=1 r_en=0 data_in=8d data_out=00 full=0 empty=0
+Time=85000 | w_en=1 r_en=0 data_in=65 data_out=00 full=0 empty=0
+Time=95000 | w_en=1 r_en=0 data_in=12 data_out=00 full=1 empty=0
+Time=105000 | w_en=0 r_en=0 data_in=12 data_out=00 full=1 empty=0
+Time=115000 | w_en=1 r_en=0 data_in=aa data_out=00 full=1 empty=0
+Reading from FIFO
+Time=125000 | w_en=0 r_en=0 data_in=aa data_out=00 full=1 empty=0
+Time=135000 | w_en=0 r_en=1 data_in=aa data_out=24 full=0 empty=0
+Time=145000 | w_en=0 r_en=1 data_in=aa data_out=81 full=0 empty=0
+Time=155000 | w_en=0 r_en=1 data_in=aa data_out=09 full=0 empty=0
+Time=165000 | w_en=0 r_en=1 data_in=aa data_out=63 full=0 empty=0
+Time=175000 | w_en=0 r_en=1 data_in=aa data_out=0d full=0 empty=0
+Time=185000 | w_en=0 r_en=1 data_in=aa data_out=8d full=0 empty=0
+Time=195000 | w_en=0 r_en=1 data_in=aa data_out=65 full=0 empty=0
+Time=205000 | w_en=0 r_en=1 data_in=aa data_out=12 full=0 empty=1
+Simultanious Read/Write
+Time=215000 | w_en=0 r_en=0 data_in=aa data_out=12 full=0 empty=1
+Time=225000 | w_en=1 r_en=1 data_in=01 data_out=12 full=0 empty=0
+Time=235000 | w_en=1 r_en=1 data_in=0d data_out=01 full=0 empty=0
+Time=245000 | w_en=1 r_en=1 data_in=76 data_out=0d full=0 empty=0
+Time=255000 | w_en=1 r_en=1 data_in=3d data_out=76 full=0 empty=0
+Time=265000 | w_en=1 r_en=1 data_in=ed data_out=3d full=0 empty=0
+Time=275000 | w_en=0 r_en=0 data_in=ed data_out=3d full=0 empty=0
+synchronous_fifo_tb.v:79: $finish called at 325000 (1ps)
+*/
